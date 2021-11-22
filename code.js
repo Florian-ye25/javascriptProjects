81f64bc451ca6a785c7d86784731577b41549acd
Florian-ye25 <florian.manival@gmail.com>
2021-11-22 15:34:48 +0100

    correction de let result en let resultat, j'ai commencé en français et il faut le garder jusqu'au bout. mise en place d'un switch qui appelle la fonction correspondante à celle choisie au premier menu.

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+let demandeOperation;
+
 do {
     demandeOperation = prompt("Quelle opération souhaitez-vous éffectuer ?\n\n"
     + "1 - Addition\n"
@@ -14,26 +16,40 @@ do {
     deuxiemeNombre = prompt("Quel est votre deuxième chiffre ou nombre ?");
 } while (isNaN(deuxiemeNombre) || deuxiemeNombre == null || deuxiemeNombre == "" || deuxiemeNombre == " ");
 
+switch (demandeOperation) {
+    case 1: addition();
+        break;
+    case 2: multiplication();
+        break;
+    case 3: soustraction();
+        break;
+    case 4: division();
+        break;
+
+        default: alert("Cette opération n'est pas disponible");
+        break;
+}
+
 function addition(nombreA, nombreB){
-    let result = nombreA + nombreB;
-    return result;
+    let resultat = nombreA + nombreB;
+    return resultat;
 }
 
 function multiplication(nombreA, nombreB){
-    let result = nombreA * nombreB;
-    return result;
+    let resultat = nombreA * nombreB;
+    return resultat;
 }
 
 function soustraction(nombreA, nombreB){
-    let result = nombreA - nombreB;
-    return result;
+    let resultat = nombreA - nombreB;
+    return resultat;
 }
 
 function division(nombreA, nombreB){
     if(nombreA == 0 || nombreB == 0){
         alert("Vous ne pouvez pas diviser par 0 !");
     }else{
-        let result = nombreA / nombreB;
-        return result;
+        let resultat = nombreA / nombreB;
+        return resultat;
     }
 }
