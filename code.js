ff9f1e0361d543a4f8eb27aaeaa0621c078194ef
Florian-ye25 <florian.manival@gmail.com>
2021-11-22 15:51:46 +0100

    Ajout d'une alerte pour afficher le résultat. Il sortait en NaN au début et après une track des variables avec des console.log, j'ai vu mon erreur.

--- code.js
+++ code.js
@@ -1,33 +1,36 @@
-let demandeOperation;
+let demandeOperation, premierNombre, deuxiemeNombre, resultat;
 
 do {
-    demandeOperation = prompt("Quelle opération souhaitez-vous éffectuer ?\n\n"
+    demandeOperation = parseInt(prompt("Quelle opération souhaitez-vous éffectuer ?\n\n"
     + "1 - Addition\n"
     + "2 - Multiplication\n"
     + "3 - Soustraction\n"
-    + "4 - Division\n");
+    + "4 - Division\n"));
 } while (demandeOperation != 1 && demandeOperation != 2 && demandeOperation !=3 && demandeOperation != 4)
 
+
 do {
-    premierNombre = prompt("Quel est votre premier chiffre ou nombre ?");
+    premierNombre = parseFloat(prompt("Quel est votre premier chiffre ou nombre ?"));
 } while (isNaN(premierNombre) || premierNombre == null || premierNombre == "" || premierNombre == " ");
 
+
 do {
-    deuxiemeNombre = prompt("Quel est votre deuxième chiffre ou nombre ?");
+    deuxiemeNombre = parseFloat(prompt("Quel est votre deuxième chiffre ou nombre ?"));
 } while (isNaN(deuxiemeNombre) || deuxiemeNombre == null || deuxiemeNombre == "" || deuxiemeNombre == " ");
 
+
 function addition(nombreA, nombreB){
-    let resultat = nombreA + nombreB;
+    resultat = nombreA + nombreB;
     return resultat;
 }
 
 function multiplication(nombreA, nombreB){
-    let resultat = nombreA * nombreB;
+    resultat = nombreA * nombreB;
     return resultat;
 }
 
 function soustraction(nombreA, nombreB){
-    let resultat = nombreA - nombreB;
+    resultat = nombreA - nombreB;
     return resultat;
 }
 
@@ -35,21 +38,23 @@ function division(nombreA, nombreB){
     if(nombreA == 0 || nombreB == 0){
         alert("Vous ne pouvez pas diviser par 0 !");
     }else{
-        let resultat = nombreA / nombreB;
+        resultat = nombreA / nombreB;
         return resultat;
     }
 }
 
 switch (demandeOperation) {
-    case 1: addition();
+    case 1: addition(premierNombre, deuxiemeNombre);
         break;
-    case 2: multiplication();
+    case 2: multiplication(premierNombre, deuxiemeNombre);
         break;
-    case 3: soustraction();
+    case 3: soustraction(premierNombre, deuxiemeNombre);
         break;
-    case 4: division();
+    case 4: division(premierNombre, deuxiemeNombre);
         break;
 
         default: alert("Cette opération n'est pas disponible");
         break;
 }
+
+alert("Le résultat de l'opération est : " + resultat + ".");
