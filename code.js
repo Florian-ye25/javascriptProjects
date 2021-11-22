26c4e444329829eeb84d066e2713a40cb156357e
Florian-ye25 <florian.manival@gmail.com>
2021-11-22 14:37:33 +0100

    modification du nom du fichier js et mise en place de la première interface de demande d'opération avec condition pour être sûr que l utilisateur entre un chiffre entre 1 et 4

---
+++ code.js
@@ -0,0 +1,7 @@
+do {
+    demandeOperation = prompt("Quelle opération souhaitez-vous éffectuer ?\n\n"
+    + "1 - Addition\n"
+    + "2 - Multiplication\n"
+    + "3 - Soustraction\n"
+    + "4 - Division\n");
+} while (demandeOperation != 1 && demandeOperation != 2 && demandeOperation !=3 && demandeOperation != 4)
--- exemple.js
+++
@@ -1,3 +0,0 @@
-let x = 5;
-
-[x > 3] ? [console.log("x est supérieur à trois")] : [console.log("x est inférieur à trois")];
